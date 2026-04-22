2f3f9e326808aace84126fca420bbe505cbd055d
Srikanth <srikanth.g@releaseowl.com>
2026-04-22 20:59:25 +0530

    remove spaces in congig history

--- karma.conf.cjs
+++ karma.conf.cjs
@@ -1,13 +1,9 @@
 module.exports = function (config) {
-
     config.set({
-
         frameworks: ["ui5"],
-
         ui5: {
             configPath: "ui5.yaml"
         },
-
         hostname: process.env.PIPER_SELENIUM_HOSTNAME || "karma",
         listenAddress: "0.0.0.0",
         port: 9876,
@@ -16,109 +12,50 @@ module.exports = function (config) {
             ChromeWebDriver: {
                 base: "WebDriver",
                 config: {
-
                     hostname: process.env.PIPER_SELENIUM_WEBDRIVER_HOSTNAME || "selenium",
-
                     port: parseInt(process.env.PIPER_SELENIUM_WEBDRIVER_PORT) || 4444
-
                 },
-
                 browserName: "chrome",
-
                 name: "Karma",
-
                 flags: [
-
                     "--no-sandbox",
-
                     "--disable-dev-shm-usage",
-
                     "--disable-gpu",
-
                     "--disable-web-security",
-
                     "--allow-running-insecure-content"
-
                 ],
-
                 pseudoActivityInterval: 30000
-
             }
-
         },
-
-
-
         reporters: ["progress", "html", "junit"],
-
-
-
         htmlReporter: {
-
             outputDir: "test-results/opa",
-
             reportName: "OPA5-Test-Report"
-
         },
-
-
-
         junitReporter: {
-
             outputDir: "reports",
-
             outputFile: "TESTS-karma.xml",
-
             useBrowserName: false,
-
             suite: "KarmaTests"
-
         },
-
-
-
         captureTimeout: 210000,
-
         browserDisconnectTimeout: 210000,
-
         browserDisconnectTolerance: 3,
-
         browserNoActivityTimeout: 210000,
-
-
-
         singleRun: true,
-
         autoWatch: false,
-
         colors: true,
-
         logLevel: config.LOG_INFO,
-
-
-
         forceJSONP: true,
-
         concurrency: 1,
-
-
-
         plugins: [
-
             "karma-ui5",
-
             "karma-qunit",
-
             "karma-chrome-launcher",
-
             "karma-html-reporter",
-
             "karma-junit-reporter",
-
             "karma-webdriver-launcher"
-
         ]
 
     });
-
 };
